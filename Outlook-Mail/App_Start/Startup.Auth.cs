59bf797c70cd1c18163adc45a16ecd60762f8d00
pruetsapon <pruetsapon@hotmail.com>
2020-01-11 00:01:35 +0700

    read outlook email.

--- Outlook-Mail/App_Start/Startup.Auth.cs
+++ Outlook-Mail/App_Start/Startup.Auth.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Configuration;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Tokens;
+using System.Threading.Tasks;
+using System.Web;
+
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.IdentityModel.Protocols;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Identity.Client;
+
 using Owin;
 using Outlook_Mail.Models;
+
+using Outlook_Mail.TokenStorage;
 
 namespace Outlook_Mail
 {
     public partial class Startup
     {
-        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public static string appId = ConfigurationManager.AppSettings["ida:AppId"];
+        public static string appPassword = ConfigurationManager.AppSettings["ida:AppPassword"];
+        public static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+        public static string[] scopes = ConfigurationManager.AppSettings["ida:AppScopes"]
+          .Replace(' ', ',').Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
@@ -63,6 +83,80 @@ namespace Outlook_Mail
             //    ClientId = "",
             //    ClientSecret = ""
             //});
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+              new OpenIdConnectAuthenticationOptions
+              {
+                  ClientId = appId,
+                  Authority = "https://login.microsoftonline.com/common/v2.0",
+                  Scope = "openid offline_access profile email " + string.Join(" ", scopes),
+                  RedirectUri = redirectUri,
+                  PostLogoutRedirectUri = "/",
+                  TokenValidationParameters = new TokenValidationParameters
+                  {
+                      // For demo purposes only, see below
+                      ValidateIssuer = false
+
+                      // In a real multitenant app, you would add logic to determine whether the
+                      // issuer was from an authorized tenant
+                      //ValidateIssuer = true,
+                      //IssuerValidator = (issuer, token, tvp) =>
+                      //{
+                      //  if (MyCustomTenantValidation(issuer))
+                      //  {
+                      //    return issuer;
+                      //  }
+                      //  else
+                      //  {
+                      //    throw new SecurityTokenInvalidIssuerException("Invalid issuer");
+                      //  }
+                      //}
+                  },
+                  Notifications = new OpenIdConnectAuthenticationNotifications
+                  {
+                      AuthenticationFailed = OnAuthenticationFailed,
+                      AuthorizationCodeReceived = OnAuthorizationCodeReceived
+                  }
+              }
+            );
+        }
+
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage,
+          OpenIdConnectAuthenticationOptions> notification)
+        {
+            notification.HandleResponse();
+            string redirect = "/Home/Error?message=" + notification.Exception.Message;
+            if (notification.ProtocolMessage != null && !string.IsNullOrEmpty(notification.ProtocolMessage.ErrorDescription))
+            {
+                redirect += "&debug=" + notification.ProtocolMessage.ErrorDescription;
+            }
+            notification.Response.Redirect(redirect);
+            return Task.FromResult(0);
+        }
+
+        private async Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedNotification notification)
+        {
+            //Get the signed in user's id and create a token cache
+            string signedInUserId = notification.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            SessionTokenCache tokenCache = new SessionTokenCache(signedInUserId,
+                notification.OwinContext.Environment["System.Web.HttpContextBase"] as HttpContextBase);
+            ConfidentialClientApplication cca = new ConfidentialClientApplication(appId, redirectUri, new ClientCredential(appPassword), tokenCache.GetMsalCacheInstance(), null);
+
+            try
+            {
+                var result = await cca.AcquireTokenByAuthorizationCodeAsync(notification.Code, scopes);
+            }
+            catch (MsalException ex)
+            {
+                string message = "AcquireTokenByAuthorizationCodeAsync threw an exception";
+                string debug = ex.Message;
+                notification.HandleResponse();
+                notification.Response.Redirect("/Home/Error?message=" + message + "&debug=" + debug);
+            }
         }
     }
 }
--- Outlook-Mail/Controllers/HomeController.cs
+++ Outlook-Mail/Controllers/HomeController.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Configuration;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Graph;
+
+using Outlook_Mail.TokenStorage;
 
 namespace Outlook_Mail.Controllers
 {
@@ -10,21 +22,135 @@ namespace Outlook_Mail.Controllers
     {
         public ActionResult Index()
         {
+            if (Request.IsAuthenticated)
+            {
+                string userName = ClaimsPrincipal.Current.FindFirst("name").Value;
+                string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+                if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(userId))
+                {
+                    // Invalid principal, sign out
+                    return RedirectToAction("SignOut");
+                }
+
+                // Since we cache tokens in the session, if the server restarts
+                // but the browser still has a cached cookie, we may be
+                // authenticated but not have a valid token cache. Check for this
+                // and force signout.
+                SessionTokenCache tokenCache = new SessionTokenCache(userId, HttpContext);
+                if (!tokenCache.HasData())
+                {
+                    // Cache is empty, sign out
+                    return RedirectToAction("SignOut");
+                }
+
+                ViewBag.UserName = userName;
+            }
             return View();
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> Inbox()
         {
-            ViewBag.Message = "Your application description page.";
+            string token = await GetAccessToken();
+            if (string.IsNullOrEmpty(token))
+            {
+                // If there's no token in the session, redirect to Home
+                return Redirect("/");
+            }
 
-            return View();
+            GraphServiceClient client = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", token);
+
+                        return Task.FromResult(0);
+                    }));
+
+            try
+            {
+                var mailResults = await client.Me.MailFolders.Inbox.Messages.Request()
+                                    .OrderBy("receivedDateTime DESC")
+                                    .Select("subject,receivedDateTime,from")
+                                    .Top(10)
+                                    .GetAsync();
+
+                return View(mailResults.CurrentPage);
+            }
+            catch (ServiceException ex)
+            {
+                return RedirectToAction("Error", "Home", new { message = "ERROR retrieving messages", debug = ex.Message });
+            }
         }
 
-        public ActionResult Contact()
+        public void SignIn()
         {
-            ViewBag.Message = "Your contact page.";
+            if (!Request.IsAuthenticated)
+            {
+                // Signal OWIN to send an authorization request to Azure
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
 
-            return View();
+        public void SignOut()
+        {
+            if (Request.IsAuthenticated)
+            {
+                string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    // Get the user's token cache and clear it
+                    SessionTokenCache tokenCache = new SessionTokenCache(userId, HttpContext);
+                    tokenCache.Clear();
+                }
+            }
+            // Send an OpenID Connect sign-out request.
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                CookieAuthenticationDefaults.AuthenticationType);
+            Response.Redirect("/");
+        }
+
+        public ActionResult Error(string message, string debug)
+        {
+            ViewBag.Message = message;
+            ViewBag.Debug = debug;
+            return View("Error");
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            string accessToken = null;
+
+            // Load the app config from web.config
+            string appId = ConfigurationManager.AppSettings["ida:AppId"];
+            string appPassword = ConfigurationManager.AppSettings["ida:AppPassword"];
+            string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+            string[] scopes = ConfigurationManager.AppSettings["ida:AppScopes"]
+                .Replace(' ', ',').Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Get the current user's ID
+            string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                // Get the user's token cache
+                SessionTokenCache tokenCache = new SessionTokenCache(userId, HttpContext);
+
+                ConfidentialClientApplication cca = new ConfidentialClientApplication(
+                    appId, redirectUri, new ClientCredential(appPassword), tokenCache.GetMsalCacheInstance(), null);
+
+                // Call AcquireTokenSilentAsync, which will return the cached
+                // access token if it has not expired. If it has expired, it will
+                // handle using the refresh token to get a new one.
+                AuthenticationResult result = await cca.AcquireTokenSilentAsync(scopes, cca.GetAccountsAsync().Result.First());
+
+                accessToken = result.AccessToken;
+            }
+
+            return accessToken;
         }
     }
 }
