9f2c13169a0feb3ed747c51cb1a91ba599dcd247
pruetsapon <pruetsapon@hotmail.com>
2020-01-10 22:40:35 +0700

    new project.

---
+++ Outlook-Mail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Outlook_Mail.Startup))]
+namespace Outlook_Mail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
